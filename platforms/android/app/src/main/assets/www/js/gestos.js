ffeb0840ec0a71bf30deab2bd1b932536a469158
DavidManzanera13 <davidmanzanera_13@outlook.com>
2019-10-16 12:23:58 -0500

    agregado hammer.js

--- platforms/android/app/src/main/assets/www/js/gestos.js
+++ platforms/android/app/src/main/assets/www/js/gestos.js
@@ -1,19 +1,46 @@
-var app={
+var app = {
     inicio: function(){
+        this.iniciaBotones();
+        this.iniciaFastClick();
+        this.iniciaHammer();
+    },
+ 
+    iniciaFastClick: function(){
+        FastClick.attach(document.body);
+    },
+ 
+    iniciaBotones: function(){
         var btnClaro = document.querySelector('#claro');
         var btnObscuro = document.querySelector('#obscuro');
  
         btnClaro.addEventListener('click',this.ponloClaro, false);
-        btnObscuro.addEventListener('click', this.ponloBobscuro, false);
-
+        btnObscuro.addEventListener('click',this.ponloBobscuro, false);
+       
     },
+ 
+    iniciaHammer: function() {
+        var zona = document.getElementById('zona-gestos');
+        var hammertime = new Hammer(zona);
+ 
+        hammertime.get('pinch').set({enable: true});
+        hammertime.get('rotate').set({enable: true});
+ 
+        hammertime.on('tap doubletap pan swipe press pinch rotate', function(ev){
+            document.querySelector('#info').innerHTML = ev.type+'!';
+        });
+    },
+ 
     ponloClaro: function(){
         document.body.className = 'claro';
     },
     ponloBobscuro: function(){
         document.body.className = 'obscuro';
     },
-
-};
-
-app.inicio();
+ };
+ 
+ if('addEventListener' in document){
+    document.addEventListener('DOMContentLoaded', function(){
+        app.inicio();
+    }, false);
+ }
+ 
